d94ef6fa90033c20dd031551836ba5f3d9788450
williambayliss <wmbayliss@gmail.com>
2021-10-26 14:20:04 -0400

    Created initial module for board with a multi-d array and a method to log the values of the cells to the console

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 var gameBoardDiv = document.getElementById('game-board');
 
+const gameBoard = (() => {
+    var board = [
+        [["X"], ["x"], ["X"]],
+        [["X"], ["X"], ["X"]],
+        [["X"], ["X"], ["X"]]
+    ];
+
+    const displayBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            subArray = board[i]
+            for (let j = 0;  j < subArray.length; j++) {
+                console.log(board[i][j])
+            }
+        }
+    };
+
+    return { displayBoard }
+})();
+
 const Player = name => {
     const sayName = () => {
         console.log(name)
     }
 
     return { sayName }
-}
+};
+
+gameBoard.displayBoard()
