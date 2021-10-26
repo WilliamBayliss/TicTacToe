3e018074322c4169e268b714528625af7dd71780
williambayliss <wmbayliss@gmail.com>
2021-10-26 15:35:13 -0400

    Created factory function for cells, added board display function to gameBoard module that will programatically create the board array and fill it with cells, as well as create the corresponding structure in the html

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-var gameBoardDiv = document.getElementById('game-board');
-
-const gameBoard = (() => {
-    var board = [
-        [["X"], ["x"], ["X"]],
-        [["X"], ["X"], ["X"]],
-        [["X"], ["X"], ["X"]]
-    ];
-
-    const displayBoard = () => {
-        for (let i = 0; i < board.length; i++) {
-            subArray = board[i]
-            for (let j = 0;  j < subArray.length; j++) {
-                console.log(board[i][j]);
-            }
-        }
-    };
-
-    return { displayBoard }
-})();
 
 const Player = name => {
     const sayName = () => {
-        console.log(name)
+        console.log(name);
     }
 
-    return { sayName }
+    return { sayName };
 };
 
-gameBoard.displayBoard()
+const Cell = (value) => {
+    
+    const setValue = (token) => {
+        value = token;
+    };
+
+    return { setValue, value }
+}
+
+const gameBoard = (() => {
+
+    const initializeBoard = () => {
+        var board = [
+            [],
+            [],
+            [],
+
+        ];
+        // Select the div for the board display
+        boardDisplay = document.getElementById("game-board")
+        // For each "row":
+        for (let i = 0; i <= 2; i++) {
+            // Create a row and append it to the board display
+            let row = document.createElement("div");
+            row.classList.add("row");
+            boardDisplay.appendChild(row);
+            // For each "cell":
+            for (let j = 0; j <= 2; j++) {
+                // Create a cell at the coordinate in the board array
+                board[i][j] = Cell("X");
+
+                // Create a cell element and append it to the html row element
+                let cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.innerHTML = board[i][j].value; 
+                row.appendChild(cell);
+            }
+        }
+        return board
+    };
+
+    return { initializeBoard }
+})();
+
+gameBoard.initializeBoard();
+
+
