8e00321ff5c1cd849220cd8d5d21c02dcbc7b7b7
williambayliss <wmbayliss@gmail.com>
2021-10-26 14:10:40 -0400

    Added factory function for player object with basic code to start

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var gameBoardDiv = document.getElementById('game-board');
 
+const Player = name => {
+    const sayName = () => {
+        console.log(name)
+    }
+
+    return { sayName }
+}
+
+player1.sayName()
