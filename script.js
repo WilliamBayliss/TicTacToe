f3a14e87b3a5a02a381263d3e079689ceb932069
williambayliss <wmbayliss@gmail.com>
2021-10-26 17:13:25 -0400

    Wrote several helper functions to assess board state

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-const Player = name => {
-    const sayName = () => {
-        console.log(name);
+const Player = (token) => {
+
+    const setToken = (string) => {
+        token = string;
     }
 
-    return { sayName };
+    return { setToken };
 };
 
 const Cell = (value, element) => {
@@ -12,9 +13,6 @@ const Cell = (value, element) => {
     const initialize = () => {
         element.innerHTML = value;
 
-        element.addEventListener("click", function(event) {
-            changeValue("X")
-        })
     };
 
     const changeValue = (token) => {
@@ -52,15 +50,52 @@ const gameBoard = (() => {
                 cell.classList.add("cell-container");
                 row.appendChild(cell);
                 // Create a cell at the coordinate in the board array
-                board[i][j] = Cell(" ", cell);
+                board[i][j] = Cell(null, cell);
             };
         };
-
+        return board;
     };
+
+    var board = initializeBoard();
+
+    // Helper functions to assess board state
+    // 
+    // Returns true if a cell value is null
+    const emptyCell = (cell) => {
+        if (cell.value !== null) {
+            return false
+        };
+        return true;
+    };
+
+
+    // Only returns true if all cells on the board are empty
+    const emptyBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (!emptyCell(board[i][j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+
+    // Gets an array of vertical cells from the board
+    const getColumnFromBoard = (index) => {
+        column = [board[0][index], board[1][index], board[2][index]]
+        return column;
+    };
+
+    // Returns true if all cells in an array have the same value
+    const allCellsSameValue = array => {
+        array.every( value => value === arr [0] );
+    };
+
+
     return { initializeBoard }
 })();
-
-gameBoard.initializeBoard();
 
 
 
